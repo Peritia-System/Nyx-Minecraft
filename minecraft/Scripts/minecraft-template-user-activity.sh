61be1762e9d2ad155380d7f971d37c7afd283528
Peritia <Peritia@peritia.space>
2025-12-22 14:35:41 +0100

    fix: awk not found

--- minecraft/Scripts/minecraft-template-user-activity.sh
+++ minecraft/Scripts/minecraft-template-user-activity.sh
@@ -1,8 +1,41 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Injected by Nix 
+RSYNC_BIN="@RSYNC_BIN@"
 DATA_DIR="@DATA_DIR@"
+MCSTATUS_BIN="@MCSTATUS_BIN@"
+MCRCON_BIN="@MCRCON_BIN@"
+AWK_BIN="@AWK_BIN@"
+QUERY_PORT="@QUERY_PORT@"
+RCON_PORT="@RCON_PORT@"
+RCON_PASSWORD="@RCON_PASSWORD@"
 SERVER_NAME="@SERVER_NAME@"
+TAR_BIN="@TAR_BIN@"
+ZIP_BIN="@ZIP_BIN@"
+UNZIP_BIN="@UNZIP_BIN@"
+GZIP_BIN="@GZIP_BIN@"
+ZSTD_BIN="@ZSTD_BIN@"
+PV_BIN="@PV_BIN@"
+DU_BIN="@DU_BIN@"
+BZIP2_BIN="@BZIP2_BIN@"
+XZ_BIN="@XZ_BIN@"
+
+# Convenience wrappers 
+rsync_cmd="$RSYNC_BIN"
+awk_cmd="$AWK_BIN"
+mcstatus_cmd="$MCSTATUS_BIN 127.0.0.1:${QUERY_PORT}"
+mcrcon_cmd="$MCRCON_BIN -H 127.0.0.1 -P ${RCON_PORT} -p ${RCON_PASSWORD}"
+tar_cmd="$TAR_BIN"
+zip_cmd="$ZIP_BIN"
+unzip_cmd="$UNZIP_BIN"
+gzip_cmd="$GZIP_BIN"
+zstd_cmd="$ZSTD_BIN"
+pv_cmd="$PV_BIN"
+du_cmd="$DU_BIN"
+bzip2_cmd="$BZIP2_BIN"
+xz_cmd="$XZ_BIN"
+
 
 # Provided by systemd Environment
 QUERY_BIN="${QUERY_BIN:-minecraft-${SERVER_NAME}-query}"
@@ -17,7 +50,7 @@ OUTPUT="$($QUERY_BIN || true)"
 
 PLAYER_LINE="$(echo "$OUTPUT" | grep '^players:' || true)"
 
-ONLINE="$(echo "$PLAYER_LINE" | awk '{print $2}' | cut -d/ -f1)"
+ONLINE="$(echo "$PLAYER_LINE" | awk_cmd '{print $2}' | cut -d/ -f1)"
 
 if [[ -z "$ONLINE" || "$ONLINE" == "0" ]]; then
   echo "[$TIMESTAMP] No user detected" >> "$ACTIVITY_FILE"
